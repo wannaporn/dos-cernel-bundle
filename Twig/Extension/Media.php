d2b5ba998c67918cef59bc21f9e4a3aee01f9ec4
ツ Liverbool <nukboon@gmail.com>
2015-07-30 14:03:33 +0700

    flexible cmf image filtering name.

--- Twig/Extension/Media.php
+++ Twig/Extension/Media.php
@@ -6,12 +6,14 @@ use Sylius\Bundle\MediaBundle\Twig\Extension\SyliusImageExtension;
 
 class Media extends SyliusImageExtension
 {
+    protected $filterPrefix = 'size_';
+
     /**
      * {@inheritdoc}
      */
     public function getFunctions() {
         return array(
-            new \Twig_SimpleFunction('twig_image_url',
+            new \Twig_SimpleFunction('ui_image_url',
                 array($this, 'getImageUrl'),
                 array('is_safe' => array('html'))
             ),
@@ -23,11 +25,35 @@ class Media extends SyliusImageExtension
      */
     public function getFilters() {
         return array(
-            new \Twig_SimpleFilter('twig_image_url',
+            new \Twig_SimpleFilter('ui_image_url',
                 array($this, 'getImageUrl'),
                 array('is_safe' => array('html'))
             ),
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getImageUrl($image, $options = array(), $default = '')
+    {
+        if (is_string($options)) {
+            // numeric only
+            if (preg_match('/^([0-9]+)$/', $options)) {
+                $options = sprintf('%s%sx%s',$this->filterPrefix, $options, $options);
+                // numeric x numeric
+            } elseif (preg_match('/^([0-9]+)x([0-9]+)$/', $options)) {
+                $options = sprintf('%s%s', $this->filterPrefix, $options);
+            } else {
+                // nothing
+            }
+
+            $options = array(
+                'imagine_filter' => $options
+            );
+        }
+
+        return parent::getImageUrl($image, $options, $default);
     }
 
     /**
