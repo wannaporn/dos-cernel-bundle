34e7f7b5d4d3140feb4f1b6a4177e3beaeb22052
ツ Liverbool <nukboon@gmail.com>
2015-11-14 02:50:52 +0700

    auto routing for static-content.

---
+++ EventListener/CmfContentAutoRouting.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace DoS\CernelBundle\EventListener;
+
+use Sylius\Component\Resource\Event\ResourceEvent;
+use Symfony\Cmf\Bundle\ContentBundle\Doctrine\Phpcr\StaticContent;
+use Symfony\Cmf\Bundle\RoutingBundle\Doctrine\Phpcr\Route;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class CmfContentAutoRouting extends ContainerAware
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function postPersist(ResourceEvent $event)
+    {
+        $object = $event->getSubject();
+
+        if (!$object instanceof StaticContent) {
+            return;
+        }
+
+        $prefix = $this->container->getParameter('cmf_routing.dynamic.persistence.phpcr.route_basepath');
+        $path = sprintf('%s/%s', $prefix, $object->getName());
+        $dm = $this->container->get('sylius.manager.route');
+
+        if ($dm->find(null, $path)) {
+            return;
+        }
+
+        /** @var Route $document */
+        $document = $this->container->get('sylius.repository.route')->createNew();
+        $document->setParentDocument($dm->find(null, $prefix));
+        $document->setName($object->getName());
+        $document->setContent($object);
+
+        $dm->persist($document);
+        $dm->flush($document);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function postUpdate(ResourceEvent $event)
+    {
+        $this->postPersist($event);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function postRemove(ResourceEvent $event)
+    {
+        $object = $event->getSubject();
+
+        if (!$object instanceof StaticContent) {
+            return;
+        }
+
+        $prefix = $this->container->getParameter('cmf_routing.dynamic.persistence.phpcr.route_basepath');
+        $path = sprintf('%s/%s', $prefix, $object->getName());
+        $dm = $this->container->get('sylius.manager.route');
+
+        if ($route = $dm->find(null, $path)) {
+            $dm->remove($route);
+            $dm->flush($route);
+        }
+    }
+}
