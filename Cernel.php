8baa6c51c3a08efe51fc7b4aa827a80ed6a21e49
ツ Liverbool <nukboon@gmail.com>
2016-04-02 19:45:02 +0700

    remove setting bundle

--- Cernel.php
+++ Cernel.php
@@ -47,7 +47,6 @@ class Cernel extends Kernel
 
             new \DoS\CernelBundle\DoSCernelBundle(),
             new \DoS\ResourceBundle\DoSResourceBundle(),
-            new \DoS\SettingsBundle\DoSSettingsBundle(),
             new \DoS\UserBundle\DoSUserBundle(),
             new \DoS\MailerBundle\DoSMailerBundle(),
             new \DoS\SMSBundle\DoSSMSBundle(),
